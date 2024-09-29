fb7b3d4251509c161b610c4594c256205c063bf7
Neil Svedberg <neil.svedberg@gmail.com>
2024-09-29 18:32:39 -0400

    Integrate connection to PostgreSQL database

---
+++ backend/app/db/session.py
@@ -0,0 +1,28 @@
+"""
+Functionality relating to initializing and connecting to the PostgreSQL database.
+"""
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session as AlchemySession
+from sqlalchemy.exc import IntegrityError
+from os import getenv
+
+# This is the database URL used to connect to the PostgreSQL database.
+DATABASE_URL = getenv("DATABASE_URL")
+
+# By using this file, we can create a launcher for connections to the database.
+engine = create_engine(DATABASE_URL)
+
+
+def init_db():
+    """Create all of the required tables in the PostgreSQL database."""
+    import jobsync.db.models
+
+    Model.metadata.create_all(bind=engine)
+
+
+class Session(AlchemySession):
+    """A scoped connection to the database, which can run queries."""
+
+    def __init__(self):
+        super().__init__(engine)
--- backend/app/routes.py
+++ backend/app/routes.py
@@ -1,6 +1,8 @@
 from app import app
+from app.db.session import Session
+from app.db.models import Clubs, Events, Users
+
 from flask import Blueprint, abort, jsonify, request
-from models import Clubs, Events, Users, db
 
 router = Blueprint('user', __name__, url_prefix='/user')
 
@@ -18,18 +20,20 @@ def home():
 
 @app.route('/clubs/new', methods=['POST'])
 def create_club():
+    session = Session()
     data = request.get_json()
     new_club = Clubs(club_name=data['club_name'], club_description=data['club_description'], club_president=data['club_president'], club_email=data['club_email'], club_tags=data['club_tags'], club_members=data['club_members'])
-    db.session.add(new_club)
-    db.session.commit()
+    session.add(new_club)
+    session.commit()
     return jsonify(new_club)
 
 @app.route('/events/new', methods=['POST'])
 def create_event():
+    session = Session()
     data = request.get_json()
     new_event = Events(event_name=data['event_name'], event_description=data['event_description'], event_date=data['event_date'], event_time=data['event_time'], event_location=data['event_location'], event_club=data['event_club'], event_tags=data['event_tags'])
-    db.session.add(new_event)
-    db.session.commit()
+    session.add(new_event)
+    session.commit()
     return jsonify(new_event)
 
 
