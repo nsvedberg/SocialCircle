f0ba2ffb94a9c3e78ec90d37874c723b6fc7fa82
Neil Svedberg <neil.svedberg@gmail.com>
2024-09-30 18:39:07 -0400

    Make it so that Flask plays nicely with React

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,12 +1,27 @@
+import os
+
 from flask import Flask
 
 from app.db.session import init_db
 
-app = Flask(__name__)
+from os import getenv
+
+if getenv("SCIRCLE_DEVELOP"):
+    app = Flask(__name__,
+                static_url_path=None,
+                static_folder="../../frontend/build")
+else:
+    app = Flask(__name__)
 
 from app import routes
+
+if getenv("SCIRCLE_DEVELOP"):
+    from app.routes import develop
 
 init_db()
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    if getenv("SCIRCLE_DEVELOP"):
+        app.run()
+    else:
+        app.run(debug=True)
