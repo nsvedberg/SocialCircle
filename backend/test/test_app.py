d0c2a4d9c9863138e18d2afee116b1602dd352a6
Luke Hopson <lphopson@icloud.com>
2024-11-28 17:07:38 -0500

    Finished tests for club, started testing for event

--- backend/test/test_app.py
+++ backend/test/test_app.py
@@ -1,5 +1,6 @@
 import pytest
 from app import app
+from datetime import datetime
 
 @pytest.fixture
 def client():
@@ -16,19 +17,67 @@ def test_fake_route(client):
     response = client.get('/boakkadawo')
     assert b'href="/favicon.ico"/><meta name="viewport"' in response.data
 
-def test_create_club(client):
-    response = client.post('/b/clubs/new', json={
+# Full testing suit for club functions, except for join club and comment
+def test_create_view_edit_delete_club(client):
+    creation_response = client.post('/b/clubs/new', json={
         'club_name': 'Test Club',
         'club_description': 'A test description',
         'club_email': 'Test email'
     })
-    response_data = response.get_json()
-    assert response_data['name'] == 'Test Club'
-    assert response_data['description'] == 'A test description'
-    assert response_data['email'] == 'Test email'
+    created_club = creation_response.get_json()
+    club_id = created_club['id'] # Save this id, we will use the same club for the edited and delete tests
+    assert created_club['club_name'] == 'Test Club'
 
     # After creating the club, that club title will be visible on the clubs page, check for that
-    response = client.get('/b/clubs')
-    assert b'Test Club' in response.data
+    view_response = client.get('/b/clubs')
+    assert b'Test Club' in view_response.data
+
+    # TODO: Comment testing
+
+    # Edit club
+    edit_response = client.put(f'/b/clubs/{club_id}', json={
+        'club_name': 'Updated Test Club',
+        'club_description': 'Updated description',
+        'club_email': 'updated@email.com'
+    })
+    assert edit_response.status_code == 200
+    updated_club = edit_response.get_json()
+    assert updated_club['club_name'] == 'Updated Test Club'
+    assert updated_club['club_description'] == 'Updated description'
+    assert updated_club['club_email'] == 'updated@email.com'
+
+    # Delete club
+    delete_response = client.delete(f'/b/clubs/{club_id}')
+    assert delete_response.status_code == 200
+    deleted_club = delete_response.get_json()
+    assert deleted_club['id'] == club_id # Check that deleted club was the club we were trying to delete lol
+    assert deleted_club['club_name'] == 'Updated Test Club' # Check that the deleted club was the club
+
+    # Now check the club no longer exists on the clubs list page
+    view_response = client.get('/b/clubs')
+    assert b'Updated Test Club' not in view_response.data 
+
+
+# Event tests (TODO: Add edit/delete like in the clubs testing above)
+def test_create_event(client):
+    event_date = datetime.now().isoformat() 
+    response = client.post('/b/events/new', json={
+        'event_name': 'Test Event',
+        'event_description': 'Test description',
+        'event_location': 'Somewhere',
+        'event_date': event_date,
+        'event_time': '14:00',
+        'event_club': 'Test club',
+        'event_tags': 'Test tag'
+    })
+    assert response.status_code == 201 # Successful creation
+    response = client.get('/b/events')
+    assert b'Test Event' in response.data # Check for newly created test event on dashboard
+
+# TODO: Profile/Login tests
+# TODO: Search bar tests
+
+
+
 
 
