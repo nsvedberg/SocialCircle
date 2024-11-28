369a484ddb73499209688886ece2ca170c3f764f
Luke Hopson <lphopson@icloud.com>
2024-11-28 03:07:07 -0500

    Create club test

--- backend/test/test_app.py
+++ backend/test/test_app.py
@@ -11,15 +11,24 @@ def test_default(client):
     response = client.get('/')
     assert response.status_code == 200   
 
-# This test fails for now, for some reason all real routes just return the errorhandler from develop.py?
-#def test_existing_route(client):
-#    response = client.get('/login')
-#    assert response.status_code == 200
-#    assert b'Log' in response.data
-
 # develop serves index.html when non-existent page, check for some of the index.html here
 def test_fake_route(client):
     response = client.get('/boakkadawo')
     assert b'href="/favicon.ico"/><meta name="viewport"' in response.data
+
+def test_create_club(client):
+    response = client.post('/b/clubs/new', json={
+        'club_name': 'Test Club',
+        'club_description': 'A test description',
+        'club_email': 'Test email'
+    })
+    response_data = response.get_json()
+    assert response_data['name'] == 'Test Club'
+    assert response_data['description'] == 'A test description'
+    assert response_data['email'] == 'Test email'
+
+    # After creating the club, that club title will be visible on the clubs page, check for that
+    response = client.get('/b/clubs')
+    assert b'Test Club' in response.data
 
 
